8e46e46222522d28e7806d343776a7a15c4f9e6a
Dmitry Moskalenko <dimamikron41@gmail.com>
2022-11-15 14:15:22 +0300

    change heroku to netlify

--- src/Store/Slices/orderSlice.js
+++ src/Store/Slices/orderSlice.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-// const URL = 'http://localhost:3000/api';
-const URL = '/api';
+const URL = 'http://localhost:5005/api';
 
 export const orderApi = createApi({
   reducerPath: 'orderApi',
--- src/Store/Slices/questionSlice.js
+++ src/Store/Slices/questionSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-// const URL = 'http://localhost:3000/api';
-const URL = '/api';
+const URL = 'http://localhost:5005/api';
+// const URL = '/api';
 
 export const questionApi = createApi({
   reducerPath: 'questionApi',
